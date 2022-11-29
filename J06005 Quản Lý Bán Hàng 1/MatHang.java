6ce6ff183e1b877dafa77161feabca4e7eacf1d3
Cường17_CC1 <72690085+CuongCC1@users.noreply.github.com>
2022-11-29 16:08:23 +0700

    Add files via upload

---
+++ J06005 Quản Lý Bán Hàng 1/MatHang.java
@@ -0,0 +1,62 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author Đặng Cường
+ */
+public class MatHang {
+    private String maMH ,tenMH ,donViTinh ;
+    private long giaMua,giaBan;
+
+    public MatHang(int maMH, String tenMH, String donViTinh, long giaMua, long giaBan) {
+        this.maMH = "MH"+String.format("%03d",maMH);
+        this.tenMH = tenMH;
+        this.donViTinh = donViTinh;
+        this.giaMua = giaMua;
+        this.giaBan = giaBan;
+    }
+
+    public String getMaMH() {
+        return maMH;
+    }
+
+    public void setMaMH(String maMH) {
+        this.maMH = maMH;
+    }
+
+    public String getTenMH() {
+        return tenMH;
+    }
+
+    public void setTenMH(String tenMH) {
+        this.tenMH = tenMH;
+    }
+
+    public String getDonViTinh() {
+        return donViTinh;
+    }
+
+    public void setDonViTinh(String donViTinh) {
+        this.donViTinh = donViTinh;
+    }
+
+    public long getGiaMua() {
+        return giaMua;
+    }
+
+    public void setGiaMua(long giaMua) {
+        this.giaMua = giaMua;
+    }
+
+    public long getGiaBan() {
+        return giaBan;
+    }
+
+    public void setGiaBan(long giaBan) {
+        this.giaBan = giaBan;
+    }
+    
+}
